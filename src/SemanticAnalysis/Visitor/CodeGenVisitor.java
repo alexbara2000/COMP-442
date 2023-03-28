a30b31cf0811481da336af5942124201c1cad07d
Alexandru Bara <alexandrubara2000@gmail.com>
2023-03-28 17:22:59 -0400

    Added arrray capabilities

--- src/SemanticAnalysis/Visitor/CodeGenVisitor.java
+++ src/SemanticAnalysis/Visitor/CodeGenVisitor.java
@@ -111,6 +111,26 @@ public class CodeGenVisitor implements Visitor{
         if(node.getChildren().size() == 1){
             node.setMoonVarName(node.getChildren().get(0).getMoonVarName());
         }
+        if(node.getChildren().size() == 2 && node.getChildren().get(1).getConcept().equals("indice")){
+            var indiceName = node.getChildren().get(1).getMoonVarName();
+            var arrName = node.getChildren().get(0).getMoonVarName();
+            String tempvar = "t"+currTempVar;
+            currTempVar++;
+
+            execCode += "\n";
+            execCode += m_mooncodeindent + "%assigning values in factor\n";
+            execCode += m_mooncodeindent + "lw r1,"+indiceName+"(r0)\n";
+            //todo change 4 for size of
+            execCode += m_mooncodeindent + "muli r2,r1,4\n";
+            execCode += m_mooncodeindent + "lw r9,"+arrName+"(r2)\n";
+            execCode += m_mooncodeindent + "sw "+tempvar+"(r0),r9\n";
+            execCode += "\n";
+
+            dataCode +=  "% space for array value\n";
+            dataCode += String.format("%-10s",tempvar) + " res 4\n";
+            execCode += "\n";
+            node.setMoonVarName(tempvar);
+        }
     }
 
     @Override
@@ -188,6 +208,9 @@ public class CodeGenVisitor implements Visitor{
         for (Node child : node.getChildren() ) {
             //make all children use this scopes' symbol table
             child.accept(this);
+        }
+        if(node.getChildren().size() == 1){
+            node.setMoonVarName(node.getChildren().get(0).getMoonVarName());
         }
     }
 
@@ -342,6 +365,46 @@ public class CodeGenVisitor implements Visitor{
             //make all children use this scopes' symbol table
             child.accept(this);
         }
+        if(node.getChildren().size() == 1){
+            node.setMoonVarName(node.getChildren().get(0).getMoonVarName());
+        }
+        else {
+            var firstName = node.getChildren().get(0).getMoonVarName();
+            var secondName = node.getChildren().get(2).getMoonVarName();
+            var operatorType = ((Token) node.getChildren().get(1).getConcept()).getType();
+            String tempvar = "t" + currTempVar;
+            currTempVar++;
+            if (operatorType == TokenType.MULT) {
+
+                execCode += "\n";
+                execCode += m_mooncodeindent + "% processing: " + tempvar + " := " + firstName + " * " + secondName + "\n";
+                execCode += m_mooncodeindent + "lw r1," + firstName + "(r0)\n";
+                execCode += m_mooncodeindent + "lw r2," + secondName + "(r0)\n";
+                execCode += m_mooncodeindent + "mul r3,r1,r2\n";
+                execCode += m_mooncodeindent + "sw " + tempvar + "(r0),r3\n";
+
+                dataCode += "% space for " + firstName + " * " + secondName + "\n";
+                dataCode += String.format("%-10s", tempvar) + " res 4\n";
+                execCode += "\n";
+
+                node.setMoonVarName(tempvar);
+            }
+            else if (operatorType == TokenType.DIV) {
+
+                execCode += "\n";
+                execCode += m_mooncodeindent + "% processing: " + tempvar + " := " + firstName + " / " + secondName + "\n";
+                execCode += m_mooncodeindent + "lw r1," + firstName + "(r0)\n";
+                execCode += m_mooncodeindent + "lw r2," + secondName + "(r0)\n";
+                execCode += m_mooncodeindent + "div r3,r1,r2\n";
+                execCode += m_mooncodeindent + "sw " + tempvar + "(r0),r3\n";
+
+                dataCode += "% space for " + firstName + " / " + secondName + "\n";
+                dataCode += String.format("%-10s", tempvar) + " res 4\n";
+                execCode += "\n";
+
+                node.setMoonVarName(tempvar);
+            }
+        }
     }
 
     @Override
@@ -395,23 +458,35 @@ public class CodeGenVisitor implements Visitor{
             //make all children use this scopes' symbol table
             child.accept(this);
         }
-        if(node.getChildren().size() >= 2 && ((Token)node.getChildren().get(1).getConcept()).getType() == TokenType.ASSIGN){
-            var assignName = ((Token)node.getChildren().get(0).getConcept()).getLexeme();
-            var nameToAssign = node.getChildren().get(2).getMoonVarName();
-            execCode += "\n";
-            execCode += m_mooncodeindent + "%assigning values\n";
-            execCode += m_mooncodeindent + "lw r9,"+nameToAssign+"(r0)\n";
-            execCode += m_mooncodeindent + "sw "+assignName+"(r0),r9\n";
-            execCode += "\n";
-
-
-//            var expression =
-//           outMoonCode.write("% processing: "  + assignName + " := " + p_node.getChildren().get(1).m_moonVarName + "\n");
-//            execCode += m_mooncodeindent + "lw " + "r1" + "," + p_node.getChildren().get(1).m_moonVarName + "(r0)\n";
-//            execCode += m_mooncodeindent + "sw " + p_node.getChildren().get(0).m_moonVarName + "(r0)," + "r1" + "\n";
+        boolean isEqualSign = false;
+        int positionOfEqual = 0;
+        for(var childs: node.getChildren()){
+            if(childs.getConcept() instanceof Token && ((Token)childs.getConcept()).getType() == TokenType.ASSIGN){
+                isEqualSign = true;
+                break;
+            }
+            positionOfEqual++;
         }
-
-
+        if(isEqualSign){
+            var assignName = node.getChildren().get(0).getMoonVarName();
+            var nameToAssign = node.getChildren().get(node.getChildren().size()-1).getMoonVarName();
+            execCode += "\n";
+            if(node.getChildren().get(1).getConcept().equals("indice")){
+                var arrayplaceName = node.getChildren().get(1).getMoonVarName();
+                execCode += m_mooncodeindent + "%assigning values\n";
+                execCode += m_mooncodeindent + "lw r1,"+arrayplaceName+"(r0)\n";
+                //todo change 4 for size of
+                execCode += m_mooncodeindent + "muli r2,r1,4\n";
+                execCode += m_mooncodeindent + "lw r9,"+nameToAssign+"(r0)\n";
+                execCode += m_mooncodeindent + "sw "+assignName+"(r2),r9\n";
+            }
+            else{
+                execCode += m_mooncodeindent + "%assigning values\n";
+                execCode += m_mooncodeindent + "lw r9,"+nameToAssign+"(r0)\n";
+                execCode += m_mooncodeindent + "sw "+assignName+"(r0),r9\n";
+            }
+            execCode += "\n";
+        }
     }
 
     @Override
@@ -430,6 +505,43 @@ public class CodeGenVisitor implements Visitor{
         }
         if(node.getChildren().size() == 1){
             node.setMoonVarName(node.getChildren().get(0).getMoonVarName());
+        }
+        else {
+            var firstName = node.getChildren().get(0).getMoonVarName();
+            var secondName = node.getChildren().get(2).getMoonVarName();
+            var operatorType = ((Token) node.getChildren().get(1).getConcept()).getType();
+            String tempvar = "t" + currTempVar;
+            currTempVar++;
+            if (operatorType == TokenType.MULT) {
+
+                execCode += "\n";
+                execCode += m_mooncodeindent + "% processing: " + tempvar + " := " + firstName + " * " + secondName + "\n";
+                execCode += m_mooncodeindent + "lw r1," + firstName + "(r0)\n";
+                execCode += m_mooncodeindent + "lw r2," + secondName + "(r0)\n";
+                execCode += m_mooncodeindent + "mul r3,r1,r2\n";
+                execCode += m_mooncodeindent + "sw " + tempvar + "(r0),r3\n";
+
+                dataCode += "% space for " + firstName + " * " + secondName + "\n";
+                dataCode += String.format("%-10s", tempvar) + " res 4\n";
+                execCode += "\n";
+
+                node.setMoonVarName(tempvar);
+            }
+            else if (operatorType == TokenType.DIV) {
+
+                execCode += "\n";
+                execCode += m_mooncodeindent + "% processing: " + tempvar + " := " + firstName + " / " + secondName + "\n";
+                execCode += m_mooncodeindent + "lw r1," + firstName + "(r0)\n";
+                execCode += m_mooncodeindent + "lw r2," + secondName + "(r0)\n";
+                execCode += m_mooncodeindent + "div r3,r1,r2\n";
+                execCode += m_mooncodeindent + "sw " + tempvar + "(r0),r3\n";
+
+                dataCode += "% space for " + firstName + " / " + secondName + "\n";
+                dataCode += String.format("%-10s", tempvar) + " res 4\n";
+                execCode += "\n";
+
+                node.setMoonVarName(tempvar);
+            }
         }
     }
 
@@ -463,20 +575,24 @@ public class CodeGenVisitor implements Visitor{
             //make all children use this scopes' symbol table
             child.accept(this);
         }
+        if(node.getChildren().size() == 1){
+            node.setMoonVarName(node.getChildren().get(0).getMoonVarName());
+            execCode += "\n";
+            execCode += m_mooncodeindent + "% processing: put("  + node.getMoonVarName() + ")\n";
+            execCode += m_mooncodeindent + "lw " + "r1" + "," + node.getMoonVarName() + "(r0)\n";
+            execCode += m_mooncodeindent + "% put value on stack\n";
+            execCode += m_mooncodeindent + "sw -8(r14)," + "r1" + "\n";
+            execCode += m_mooncodeindent + "% link buffer to stack\n";
+            execCode += m_mooncodeindent + "addi " + "r1" + ",r0, buf\n";
+            execCode += m_mooncodeindent + "sw -12(r14)," + "r1" + "\n";
+            execCode += m_mooncodeindent + "% convert int to string for output\n";
+            execCode += m_mooncodeindent + "jl r15, intstr\n";
+            execCode += m_mooncodeindent + "sw -8(r14),r13\n";
+            execCode += m_mooncodeindent + "% output to console\n";
+            execCode += m_mooncodeindent + "jl r15, putstr\n";
+            execCode += "\n";
+        }
 
-        execCode += "\n";
-        execCode += m_mooncodeindent + "% processing: put("  + currNode.getMoonVarName() + ")\n";
-        execCode += m_mooncodeindent + "lw " + "r1" + "," + currNode.getMoonVarName() + "(r0)\n";
-        execCode += m_mooncodeindent + "% put value on stack\n";
-        execCode += m_mooncodeindent + "sw -8(r14)," + "r1" + "\n";
-        execCode += m_mooncodeindent + "% link buffer to stack\n";
-        execCode += m_mooncodeindent + "addi " + "r1" + ",r0, buf\n";
-        execCode += m_mooncodeindent + "sw -12(r14)," + "r1" + "\n";
-        execCode += m_mooncodeindent + "% convert int to string for output\n";
-        execCode += m_mooncodeindent + "jl r15, intstr\n";
-        execCode += m_mooncodeindent + "sw -8(r14),r13\n";
-        execCode += m_mooncodeindent + "% output to console\n";
-        execCode += m_mooncodeindent + "jl r15, putstr\n";
-        execCode += "\n";
+
     }
 }
